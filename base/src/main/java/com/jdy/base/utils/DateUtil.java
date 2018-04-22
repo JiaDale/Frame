f37381d68fcc971ca0a5a8b2a023e751caf05eec
Dale <498971746@qq.com>
2018-04-22 19:08:28 +0800

    修复DateUtil

--- base/src/main/java/com/jdy/base/utils/DateUtil.java
+++ base/src/main/java/com/jdy/base/utils/DateUtil.java
@@ -4,163 +4,139 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
-
-/**
- * Created by Dale on 2017/9/25.
- * 项目名：xxx
- * 描述：时间格式转换器
- * 1.将sql date 转换成 util date
- * 2.将 util date 转换成 sql date
- * 3.将 sql date 转换成 String
- * 4.将 util date 转换成 String
- */
+import java.util.regex.Pattern;
 
 public class DateUtil {
-    private static String defaultFormat = "yyyy-MM-dd";
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat(defaultFormat, Locale.CHINA);
+    private static String format = "yyyy-MM-dd";
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.CHINA);
 
-    /**
-     * 将sql date 转换成 util date
-     *
-     * @param date sql date
-     * @return util date
-     */
-    public static Date transfrom(java.sql.Date date) {
+    public static Date convert(java.sql.Date date) {
         return new Date(date.getTime());
     }
 
-    /**
-     * 将 util date 转换成 sql date
-     *
-     * @param date util date
-     * @return sql date
-     */
-    public static java.sql.Date transfrom(Date date) {
+    public static java.sql.Date convert(Date date) {
         return new java.sql.Date(date.getTime());
     }
 
-    /**
-     * 将sql date 转换成 String
-     *
-     * @param date sql date
-     * @return String
-     */
     public static String dateFotmat(java.sql.Date date) {
-        return dateFotmat(transfrom(date));
+        return dateFormat(convert(date));
     }
 
-    /**
-     * 将 util date 转换成 String
-     *
-     * @param date util date
-     * @return String
-     */
-    public static String dateFotmat(Date date) {
-        // 设置了默认格式
-        return dateFotmat("yyyy-MM-dd", date);
+    public static String dateFormat(Date date) {
+        return dateFormat(format, date);
     }
 
-    /**
-     * 将 util date 按照format转换成 String
-     *
-     * @param format
-     * @param date
-     * @return
-     */
-    public static String dateFotmat(String format, Date date) {
+    public static String dateFormat(String format, Date date) {
         if (format == null) {
-            return "unknown";
+            throw new NullPointerException("The format string is Null!");
         }
-        dateFormat.applyPattern(getFormat(format));
-        return dateFormat.format(date);
+        simpleDateFormat.applyPattern(getFormat(format));
+        return simpleDateFormat.format(date);
     }
 
     public static String dateFormat(Object value) {
         if (value instanceof Date) {
-            return dateFormat.format(value);
+            return simpleDateFormat.format(value);
         }
         return "unknown";
     }
 
-    /**
-     * 将string 转换为util Date
-     *
-     * @param datesString
-     * @return
-     */
-    public static Date convert(String datesString) {
+    public static Date convert(String dateString) {
         try {
-            dateFormat.applyPattern(getFormat(datesString));
-            return dateFormat.parse(datesString);
+            simpleDateFormat.applyPattern(getFormat(dateString));
+            return simpleDateFormat.parse(dateString);
         } catch (ParseException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    /**
-     * @param format
-     * @return
-     */
+    public static boolean isContainsChinese(String chinese) {
+        return Pattern.compile("[\u4e00-\u9fa5]").matcher(chinese).find();
+    }
+
+
     private static String getFormat(String format) {
-        String year = "yyyy", month = "MM", day = "dd", separator;
-        separator = format.substring(4, 5);
-        if ("年".equals(separator)) {
-            defaultFormat = year + "年" + month + "月" + day + "日";
-            getMoreAccuracy(format, true, 1);
+        if (format == null) {
+            throw new NullPointerException("The format string is Null");
+        }
+
+        if (isContainsChinese(format))
+            setChineseFormat(format);
+        else {
+            getOtherFormat(format);
+        }
+        return DateUtil.format;
+    }
+
+    private static void getOtherFormat(String format) {
+        String separator;
+        StringBuffer tempFormat = new StringBuffer();
+        if (format.length() > 4) {
+            separator = format.substring(4, 5);
+            tempFormat.append("yyyy");
         } else {
-            defaultFormat = year + separator + month + separator + day;
-            getMoreAccuracy(format, false, 1);
-        }
-        return defaultFormat;
-    }
-
-    private static void getMoreAccuracy(String format, boolean isChinese, int more) {
-        String hour = "HH", minute = "mm", second = "ss";
-        if (defaultFormat.compareTo(format) < 0) {
-            switch (more) {
-                case 1:
-                    defaultFormat += " " + hour + (isChinese ? "时" : "");
-                    getMoreAccuracy(format, isChinese, 2);
-                    break;
-                case 2:
-                    defaultFormat += (isChinese ? "" : ":") + minute + (isChinese ? "分" : "");
-                    getMoreAccuracy(format, isChinese, 3);
-                    break;
-                case 3:
-                    defaultFormat += (isChinese ? "" : ":") + second + (isChinese ? "秒" : ":");
-                    break;
+            for (int i = 0; i < format.length(); i++) {
+                tempFormat.append("y");
             }
+            DateUtil.format = tempFormat.toString();
+            return;
         }
+
+        if (tempFormat.length() < format.length()) {
+            tempFormat.append(separator + "MM");
+        }
+
+        if (tempFormat.length() < format.length()) {
+            tempFormat.append(separator + "dd");
+        }
+
+        if (tempFormat.length() < format.length()) {
+            tempFormat.append(" HH");
+        }
+
+        if (tempFormat.length() < format.length()) {
+            tempFormat.append(" :mm");
+        }
+
+        if (tempFormat.length() < format.length()) {
+            tempFormat.append(" :ss");
+        }
+        DateUtil.format = tempFormat.toString();
+    }
+
+    private static void setChineseFormat(String format) {
+        if (format.contains("年"))
+            DateUtil.format = "yyyy年";
+
+        if (format.contains("月"))
+            DateUtil.format += "MM月";
+
+        if (format.contains("日") || format.contains("号"))
+            DateUtil.format += "dd日 ";
+
+        if (format.contains("时"))
+            DateUtil.format += "dd时 ";
+
+        if (format.contains("分"))
+            DateUtil.format += "dd分 ";
+
+        if (format.contains("秒"))
+            DateUtil.format += "dd秒 ";
+    }
+
+    public static String dateFormat(String current, String target) {
+        if (current == null || target == null) {
+            throw new NullPointerException("The current format or target format is Null!");
+        }
+
+        return dateFormat(target, convert(current));
     }
 
 
-    /**
-     * 将一种格式转换为另一种格式
-     *
-     * @param timestamp
-     * @return
-     */
-    public static String dateFormat(String timestamp) {
-        if (timestamp == null) {
-            return "unknown";
-        }
-        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SS");
-        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            Date date = inputFormat.parse(timestamp);
-            return outputFormat.format(date);
-        } catch (ParseException e) {
-            return "unknown";
-        }
-    }
-
-    /**
-     * 验证字符串是否是一个合法的日期格式
-     */
     public static boolean isValidDate(String format, String... dates) {
         boolean convertSuccess = true;
+
         if (dates == null || dates.length == 0) {
             return false;
         }
@@ -175,19 +151,13 @@ public class DateUtil {
     }
 
 
-    /**
-     * 验证字符串是否是一个合法的日期格式
-     */
     public static boolean isValidDate(String date, String format) {
         boolean convertSuccess = true;
-        // 指定日期格式
-        dateFormat = new SimpleDateFormat(getFormat(format), Locale.CHINA);
+        simpleDateFormat = new SimpleDateFormat(getFormat(format), Locale.CHINA);
         try {
-            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2015/02/29会被接受，并转换成2015/03/01
-            dateFormat.setLenient(false);
-            dateFormat.parse(date);
+            simpleDateFormat.setLenient(false);
+            simpleDateFormat.parse(date);
         } catch (Exception e) {
-            // 如果throw java.text.ParseException或者NullPointerException，就说明格式不对
             convertSuccess = false;
         }
         return convertSuccess;
