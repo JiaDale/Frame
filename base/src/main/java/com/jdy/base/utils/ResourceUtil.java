daebe14ac0a71d2c86a70ce57e632f29c21fb016
Dale <498971746@qq.com>
2018-04-25 00:16:20 +0800

    Add a new part to adapter so that it can be adapted to objects that are not arrays or collections.

--- base/src/main/java/com/jdy/base/utils/ResourceUtil.java
+++ base/src/main/java/com/jdy/base/utils/ResourceUtil.java
@@ -2,25 +2,31 @@ package com.jdy.base.utils;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.res.Resources.Theme;
+import android.graphics.drawable.Drawable;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.view.View;
 
 public class ResourceUtil {
     // R文件的对象
     private static Resources resources;
     private static String pkgName;
+    private static Theme mTheme;
 
 
     public static void init(Context context) {
+        mTheme = context.getTheme();
         pkgName = context.getPackageName();
         resources = context.getResources();
     }
 
-    public static int checkExistID(int id){
+    public static int checkExistID(int id) {
         return checkExistID(id, "Couldn't find the ID that for the View!");
     }
 
-    public static int checkExistID(int id, String message){
-        if (id == View.NO_ID){
+    public static int checkExistID(int id, String message) {
+        if (id == View.NO_ID) {
             throw new NullPointerException(message);
         }
         return id;
@@ -31,11 +37,26 @@ public class ResourceUtil {
     }
 
     public static int getColor(int resId) {
-        return checkResourceNotNull(resources).getColor(resId);
+        if (VERSION.SDK_INT >= VERSION_CODES.M) {
+            return checkResourceNotNull(resources).getColor(resId, mTheme);
+        } else
+            return checkResourceNotNull(resources).getColor(resId);
     }
 
     public static float getDimen(int resId) {
         return checkResourceNotNull(resources).getDimension(resId);
+    }
+
+    public static Drawable getDrawable(int resId) {
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            return checkResourceNotNull(resources).getDrawable(resId, mTheme);
+        } else {
+            return checkResourceNotNull(resources).getDrawable(resId);
+        }
+    }
+
+    public static Theme getTheme() {
+        return mTheme;
     }
 
     private static Resources checkResourceNotNull(Resources resources) {
